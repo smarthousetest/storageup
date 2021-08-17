4b8074526247b2515c1fdf9b2f0e8b35675a594e
Sergo <s.401n@mail.ru>
2021-08-17 16:25:25 +0300

    added some colors

--- lib/theme.dart
+++ lib/theme.dart
@@ -2,4 +2,13 @@ import 'package:flutter/material.dart';
 
 final kDarkTheme = ThemeData.dark().copyWith();
 
-final kLightTheme = ThemeData.light().copyWith();
+final kLightTheme = ThemeData.light().copyWith(
+  accentColor: Color(0xFF64AEEA),
+  primaryColor: Color(0xFFFFFFFF),
+  disabledColor: Color(0xFF7D7D7D),
+  textTheme: ThemeData.light().textTheme.copyWith(
+        headline1: TextStyle(
+          color: Color(0xFFC4C4C4),
+        ),
+      ),
+);
