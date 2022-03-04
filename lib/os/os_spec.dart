9094d03ec1c87eb800022f8e5c3b2d560ea4e864
alex <a99som@yandex.ru>
2022-03-04 18:51:32 +0300

    add os shell to project

---
+++ lib/os/os_spec.dart
@@ -0,0 +1,15 @@
+abstract class OsSpecifications {
+  int killProcess(String processName);
+
+  int startProcess(String processName, bool hide, [List<String> args = const []]);
+
+  String getAppLocation();
+
+  String setVersion(String version);
+
+  int registerAppInOs(String appDirPath);
+
+  int createShortcuts(String appDirPath);
+
+  void startProcessDetach(String processName, bool hide, [List<String> args = const []]);
+}
