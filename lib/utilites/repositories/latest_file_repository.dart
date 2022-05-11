f5c01f3763cea9c36e5b5bd8ddeb055e5d3ce679
alex <a99som@yandex.ru>
2022-05-11 19:11:31 +0300

    change to upstorage.net

--- lib/utilites/repositories/latest_file_repository.dart
+++ lib/utilites/repositories/latest_file_repository.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/widgets.dart';
-import 'package:hive/hive.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:injectable/injectable.dart';
 import 'package:path_provider/path_provider.dart';
