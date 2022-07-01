d21c9843bc676139ebe96655d3aba11d7fec3671
Oleg <upliftingpark@gmail.com>
2022-07-01 13:16:12 +0300

    fix save language and terms of service

--- lib/utilities/state_containers/state_container.dart
+++ lib/utilities/state_containers/state_container.dart
@@ -2,6 +2,7 @@ import 'dart:developer';
 
 import 'package:flutter/material.dart';
 import 'package:storageup/components/custom_button_template.dart';
+import 'package:storageup/utilities/language_locale.dart';
 
 class StateContainer extends StatefulWidget {
   final Widget child;
@@ -47,7 +48,9 @@ class StateContainerState extends State<StateContainer> {
     });
   }
 
-  void changeLocale(Locale newLocale) {
+  Future<void> changeLocale(Locale newLocale) async {
+    await setLocale(newLocale.languageCode);
+
     setState(() {
       _locale = newLocale;
     });
