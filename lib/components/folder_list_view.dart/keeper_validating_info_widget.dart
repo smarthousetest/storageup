81b3d2158a2fc470ed24201d65a6cc439d69822f
Oleg <upliftingpark@gmail.com>
2022-07-14 10:23:22 +0300

    add keeper validating info widget

---
+++ lib/components/folder_list_view.dart/keeper_validating_info_widget.dart
@@ -0,0 +1,125 @@
+import 'package:custom_pop_up_menu/custom_pop_up_menu.dart';
+import 'package:flutter/material.dart';
+import 'package:storageup/components/folder_list_view.dart/validate_keeper_progress_inidcator.dart';
+import 'package:storageup/constants.dart';
+import 'package:storageup/generated/l10n.dart';
+import 'package:storageup/models/keeper/keeper.dart';
+import 'package:storageup/utilities/injection.dart';
+
+class KeeperValidatingInfoWidget extends StatefulWidget {
+  const KeeperValidatingInfoWidget({
+    Key? key,
+    required this.keeper,
+    required this.localPath,
+    required this.popupControllers,
+  }) : super(key: key);
+
+  final Keeper keeper;
+  final String localPath;
+  final List<CustomPopupMenuController> popupControllers;
+
+  @override
+  State<KeeperValidatingInfoWidget> createState() =>
+      _KeeperValidatingInfoWidgetState();
+}
+
+class _KeeperValidatingInfoWidgetState
+    extends State<KeeperValidatingInfoWidget> {
+  final translate = getIt<S>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 354,
+      height: 345,
+      decoration: BoxDecoration(
+        border: Border.all(
+          color: Theme.of(context).dividerColor,
+          width: 2,
+        ),
+        color: Theme.of(context).primaryColor,
+        borderRadius: BorderRadius.circular(12),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(20, 20, 20, 9),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              children: [
+                Container(
+                  constraints: BoxConstraints(maxWidth: 200),
+                  child: Text(
+                    widget.keeper.name!,
+                    overflow: TextOverflow.ellipsis,
+                    style: TextStyle(
+                      color: Theme.of(context).focusColor,
+                      fontFamily: kNormalTextFontFamily,
+                      fontSize: 18,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Container(
+              constraints: BoxConstraints(maxWidth: 310),
+              padding: const EdgeInsets.only(top: 6, bottom: 10),
+              child: Text(
+                widget.localPath,
+                overflow: TextOverflow.ellipsis,
+                style: TextStyle(
+                  color: Theme.of(context).colorScheme.onBackground,
+                  fontFamily: kNormalTextFontFamily,
+                  fontSize: 14,
+                ),
+              ),
+            ),
+            Text(
+              'На данный момент проверяется надежность вашего принимающего устройства. Проверка может занять до 3 часов.',
+              style: TextStyle(
+                color: Theme.of(context).disabledColor,
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Center(
+              child: Container(
+                height: 140,
+                width: 140,
+                child: ValidateKeeperProgressIndicator(
+                  value: 75,
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            FractionallySizedBox(
+              widthFactor: 1,
+              child: Column(
+                children: [
+                  Text(
+                    'Оставшееся время проверки',
+                    style: TextStyle(
+                      color: Theme.of(context).disabledColor,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  Text(
+                    '5 часов 23 минуты',
+                    style: TextStyle(
+                        fontSize: 16,
+                        color: Theme.of(context).textTheme.headline2?.color),
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/components/folder_list_view.dart/validate_keeper_progress_inidcator.dart
@@ -0,0 +1,138 @@
+import 'package:flutter/material.dart';
+import 'dart:math' as math;
+
+class _ValidateKeeperArcIndicator extends CustomPainter {
+  final bool isBackGround;
+  final Color color;
+  final double? value;
+
+  _ValidateKeeperArcIndicator({
+    required this.isBackGround,
+    required this.color,
+    required this.value,
+  });
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    const arcCenter = Offset(70, 70);
+    final arcRect = Rect.fromCircle(center: arcCenter, radius: 70);
+    final startAngle = -math.pi;
+    final sweepAngle = value != null ? value : math.pi;
+    final useCenter = false;
+    final paint = Paint()
+      ..strokeCap = StrokeCap.round
+      ..color = color
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 4;
+
+    if (isBackGround) {}
+    // Offset startingPoint = Offset(0, size.height / 2);
+    // Offset endingPoint = Offset(size.width, size.height / 2);
+
+    // canvas.drawLine(startingPoint, endingPoint, paint);
+
+    canvas.drawArc(arcRect, startAngle, sweepAngle!, useCenter, paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
+  }
+}
+
+class ValidateKeeperProgressIndicator extends StatefulWidget {
+  final double value;
+
+  ValidateKeeperProgressIndicator({required this.value, Key? key})
+      : super(key: key);
+
+  @override
+  _CircularArc createState() => _CircularArc();
+}
+
+class _CircularArc extends State<ValidateKeeperProgressIndicator>
+    with SingleTickerProviderStateMixin {
+  late Animation<double> animation;
+  late AnimationController controller;
+
+  //final double value;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = AnimationController(
+      duration: Duration(seconds: 3),
+      vsync: this,
+    );
+    final curvedAnimation = CurvedAnimation(
+      parent: controller,
+      curve: Curves.easeInOutCubic,
+    );
+
+    animation =
+        Tween<double>(begin: 0.0, end: widget.value).animate(curvedAnimation)
+          ..addListener(() {
+            setState(() {});
+          });
+  }
+
+  Widget _smail(double value) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        FractionallySizedBox(
+            widthFactor: 1,
+            child: Text(
+              '${value.ceil()}%',
+              textAlign: TextAlign.center,
+              style:
+                  TextStyle(color: Theme.of(context).splashColor, fontSize: 26),
+            )),
+        FractionallySizedBox(
+            widthFactor: 1,
+            child: Text(
+              'Проверено',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Theme.of(context).textTheme.subtitle1?.color,
+              ),
+            )),
+      ],
+    );
+  }
+
+  Color _colorIndicator(double value) {
+    return Theme.of(context).splashColor;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 140,
+      height: 75,
+      child: Stack(
+        children: [
+          CustomPaint(
+            painter: _ValidateKeeperArcIndicator(
+              isBackGround: false,
+              color: Theme.of(context).canvasColor,
+              value: (100 * 3.14 * 2) / 100,
+            ),
+          ),
+          CustomPaint(
+            painter: _ValidateKeeperArcIndicator(
+              isBackGround: false,
+              color: _colorIndicator(widget.value),
+              value: (widget.value * 3.14 * 2) / 100,
+            ),
+          ),
+          Align(
+            alignment: FractionalOffset.center,
+            child: _smail(widget.value),
+          ),
+        ],
+      ),
+    );
+  }
+}
